eb66a566377ee87c637becdd0873a5028f8b0b62
TheLegend2770 <48347229+TheLegend2770@users.noreply.github.com>
2022-10-12 09:58:38 +1100

    Add files via upload

--- json/cheese_award_table.js
+++ json/cheese_award_table.js
@@ -1,4 +1,4 @@
 var vg_1 = "json/Week_10_hw.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("#high_bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
--- json/lowest_cheese_award_table.js
+++ json/lowest_cheese_award_table.js
@@ -1,4 +1,4 @@
 var vg_1 = "json/lowest_countries.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("#low_bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
