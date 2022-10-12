07065a6515871d71574cba0dc960de9895bcacce
TheLegend2770 <48347229+TheLegend2770@users.noreply.github.com>
2022-10-12 14:55:18 +1100

    Update H2S_plot.js

--- json/H2S_plot.js
+++ json/H2S_plot.js
@@ -1,4 +1,4 @@
 var vg_1 = "json/cheese_taste_scatter_H2S.vg.json";
-vegaEmbed("#H2S_plot", vg_1).then(function(result) {
+vegaEmbed("#H2S_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
