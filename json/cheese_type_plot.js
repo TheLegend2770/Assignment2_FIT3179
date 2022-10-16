d992abb2e6a947f4bf4678d6aaf6b6127f18a878
TheLegend2770 <48347229+TheLegend2770@users.noreply.github.com>
2022-10-16 16:35:48 +1100

    Update cheese_type_plot.js

--- json/cheese_type_plot.js
+++ json/cheese_type_plot.js
@@ -1,4 +1,4 @@
-var vg_1 = "json/cheese_taste_scatter_acetic.vg.json";
-vegaEmbed("#acetic_plot", vg_1).then(function(result) {
+var vg_1 = "json/cheese_type_piechart.vg.json";
+vegaEmbed("#cheese_type", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
