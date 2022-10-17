b739774fe2608f8a693cb0550b7c068980053568
TheLegend2770 <48347229+TheLegend2770@users.noreply.github.com>
2022-10-17 21:54:41 +1100

    Update cheese_award_table.js

--- json/cheese_award_table.js
+++ json/cheese_award_table.js
@@ -1,4 +1,4 @@
-var vg_1 = "json/Week_10_hw.vg.json";
+var vg_1 = "json/highest_cheese_awards.vg.json";
 vegaEmbed("#high_bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
